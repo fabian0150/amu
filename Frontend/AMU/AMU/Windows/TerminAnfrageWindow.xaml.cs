1af0245c417f28925e29be7527c8ecac04b51b51
Robin <robin.stehrlein@gmail.com>
2019-03-18 00:29:58 +0100

    Changes TerminAnfrage Bandbesetzung
    Abfragen für die einzelnen Checkboxen

--- Frontend/AMU/AMU/Windows/TerminAnfrageWindow.xaml.cs
+++ Frontend/AMU/AMU/Windows/TerminAnfrageWindow.xaml.cs
@@ -3,6 +3,7 @@ using AMU.Windows;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -34,42 +35,87 @@ namespace AMU
             lstbx_gruppen_verfuegbar.SelectionMode = SelectionMode.Extended;
         }
 
-        private void Suche_Gruppen_Clicked(object sender, RoutedEventArgs e)
+        private void GruppenSuche(object sender, RoutedEventArgs e)
         {
             lstbx_gruppen_verfuegbar.Items.Clear();
-            //Hier werden die Bands gesucht und angezeigt sobald die Abfrage fertig ist.
-            LoadAvailableBands();
-        }
-        private void LoadAvailableBands()//Lädt Bands, die den Kriterien entsprechen (Anzahl BandMembers & freier Termin)
-        {
-            List<Band> bandList = new List<Band>();
-            JArray arrayJSON = GET_Request($"https://amu.tkg.ovh/json/band/_getBands.php", "");
 
-            //JArray arrayJSON = GET_Request($"https://amu.tkg.ovh/json/band/_getAvailableBands.php?members_cnt="+1 +"&date=2019-03-02", "");
-            Band band;
-            for (int i = 0; i < arrayJSON.Count; i++)
+            if (datePickerAnfrage.SelectedDate == null)
             {
-                JObject item = (JObject)arrayJSON[i];
-
-                band = new Band
+                //Fehlerfenster anzeigen
+                return;
+            }
+            else
+            {
+                //Hier werden die Bands gesucht und angezeigt sobald die Abfrage fertig ist.
+                if (chckbxAlleinunterhalter.IsChecked == true)
                 {
-                    Name = (string)item.GetValue("name"),
-                    Logo_Path = (string)item.GetValue("logo_path"),
-                    Website_Url = (string)item.GetValue("website_url"),
-                    Notes = (string)item.GetValue("notes"),
-                    Leader_Username = (string)item.GetValue("leader_username"),
-                    Record_Date = (DateTime)item.GetValue("record_date"),
-                    ID = item.Value<int?>("ID") ?? -1,
-                    Leader_ID = item.Value<int?>("leader_id") ?? -1
-                };
-                
-                bandList.Add(band);
-                lstbx_gruppen_verfuegbar.Items.Add(band);
+                    string date = DateTime.ParseExact(datePickerAnfrage.Text, "dd.MM.yyyy",
+                                CultureInfo.InvariantCulture
+                                ).ToString("yyyy-MM-dd");
+                    LoadAvailableBands(1, date);
+                }
+                if (chckbxDuo.IsChecked == true)
+                {
+                    string date = DateTime.ParseExact(datePickerAnfrage.Text, "dd.MM.yyyy",
+                                CultureInfo.InvariantCulture
+                                ).ToString("yyyy-MM-dd");
+                    LoadAvailableBands(2, date);
+                }
+                if (chckbxTrio.IsChecked == true)
+                {
+                    string date = DateTime.ParseExact(datePickerAnfrage.Text, "dd.MM.yyyy",
+                                CultureInfo.InvariantCulture
+                                ).ToString("yyyy-MM-dd");
+                    LoadAvailableBands(3, date);
+                }
+                if (chckbxQuartett.IsChecked == true)
+                {
+                    string date = DateTime.ParseExact(datePickerAnfrage.Text, "dd.MM.yyyy",
+                                CultureInfo.InvariantCulture
+                                ).ToString("yyyy-MM-dd");
+                    LoadAvailableBands(4, date);
+                }
+                if (chckbxQuintettUndMehr.IsChecked == true)
+                {
+                    string date = DateTime.ParseExact(datePickerAnfrage.Text, "dd.MM.yyyy",
+                                CultureInfo.InvariantCulture
+                                ).ToString("yyyy-MM-dd");
+                    LoadAvailableBands(5, date);
+                }
             }
 
 
 
         }
+        private void LoadAvailableBands(int bandMembersNr, string date)//Lädt Bands, die den Kriterien entsprechen (Anzahl BandMembers & freier Termin)
+        {
+            JArray arrayJSON = GET_Request($"https://amu.tkg.ovh/json/band/_getAvailableBands.php?members_cnt=" + bandMembersNr + "&date=", date);
+            for (int i = 0; i < arrayJSON.Count; i++)
+            {
+                JObject item = (JObject)arrayJSON[i];
+                LoadBand(item);
+            }
+        }
+
+        private void LoadBand(JObject item)
+        {
+            JArray arrayJSON = GET_Request($"https://amu.tkg.ovh/json/band/_getBand.php?id=", (string)item.GetValue("ID"));
+            JObject bandItem = (JObject)arrayJSON[0];
+            Band band = new Band
+            {
+                Name = (string)bandItem.GetValue("name"),
+                Logo_Path = (string)bandItem.GetValue("logo_path"),
+                Website_Url = (string)bandItem.GetValue("website_url"),
+                Notes = (string)bandItem.GetValue("notes"),
+                Leader_Username = (string)bandItem.GetValue("leader_username"),
+                Record_Date = (DateTime)bandItem.GetValue("record_date"),
+                ID = bandItem.Value<int?>("ID") ?? -1,
+                Leader_ID = bandItem.Value<int?>("leader_id") ?? -1
+            };
+            bandList.Add(band);
+            lstbx_gruppen_verfuegbar.Items.Add(band);
+        }
+
         private JArray GET_Request(string url, string parameter)
         {
             var rawJSON = new WebClient().DownloadString(url + parameter);
@@ -79,40 +125,42 @@ namespace AMU
 
         private void Lstbx_gruppen_verfuegbar_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            lbl_bandname.Content = "";
-            lbl_bandmembers.Content = "";
-            lbl_contact_person_name.Content = "";
-            lbl_phone_number.Content = "";
-            lbl_email.Content = "";
-            lbl_address.Content = "";
-            txtblck_notes.Text = "";
-            Band band = (Band)lstbx_gruppen_verfuegbar.SelectedItem;
-            JArray arrayJSON = GET_Request("https://amu.tkg.ovh/json/user/_getUser.php?id=", band.Leader_ID.ToString());
-            User user;
-
-            JObject item = (JObject)arrayJSON[0];
-            user = new User
+            if (!(lstbx_gruppen_verfuegbar.SelectedItem == null))
             {
-                ID = item.Value<int?>("ID") ?? -1,
-                Name = (string)item.GetValue("name"),
-                Mail = (string)item.GetValue("mail"),
-                Phone_Number = (string)item.GetValue("phone_number"),
-                Address = (string)item.GetValue("address"),
-                User_Description = (string)item.GetValue("user_description"),
-                Username = (string)item.GetValue("username"),
-                User_Type = item.Value<int?>("user_type") ?? -1,
-                Notes = (string)item.GetValue("notes"),
-                Record_Date = (DateTime)item.GetValue("record_date")
-            };
+                lbl_bandname.Content = "";
+                lbl_bandmembers.Content = "";
+                lbl_contact_person_name.Content = "";
+                lbl_phone_number.Content = "";
+                lbl_email.Content = "";
+                lbl_address.Content = "";
+                txtblck_notes.Text = "";
+                Band band = (Band)lstbx_gruppen_verfuegbar.SelectedItem;
+                JArray arrayJSON = GET_Request("https://amu.tkg.ovh/json/user/_getUser.php?id=", band.Leader_ID.ToString());
+                User user;
 
-            lbl_bandname.Content = band.Name;
-            lbl_bandmembers.Content = 0;
-            lbl_contact_person_name.Content = user.Name;
-            lbl_phone_number.Content = user.Phone_Number;
-            lbl_email.Content = user.Mail;
-            lbl_address.Content = user.Address;
-            txtblck_notes.Text = user.Notes;
+                JObject item = (JObject)arrayJSON[0];
+                user = new User
+                {
+                    ID = item.Value<int?>("ID") ?? -1,
+                    Name = (string)item.GetValue("name"),
+                    Mail = (string)item.GetValue("mail"),
+                    Phone_Number = (string)item.GetValue("phone_number"),
+                    Address = (string)item.GetValue("address"),
+                    User_Description = (string)item.GetValue("user_description"),
+                    Username = (string)item.GetValue("username"),
+                    User_Type = item.Value<int?>("user_type") ?? -1,
+                    Notes = (string)item.GetValue("notes"),
+                    Record_Date = (DateTime)item.GetValue("record_date")
+                };
 
+                lbl_bandname.Content = band.Name;
+                lbl_bandmembers.Content = 0;
+                lbl_contact_person_name.Content = user.Name;
+                lbl_phone_number.Content = user.Phone_Number;
+                lbl_email.Content = user.Mail;
+                lbl_address.Content = user.Address;
+                txtblck_notes.Text = user.Notes;
+            }
 
         }
 
